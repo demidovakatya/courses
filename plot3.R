30c50d10267d72e3fb2ce04bad2de0517ca33f3d
Katya Demidova <demidovakatya@gmail.com>
2015-11-05 12:35:03 +0300

    upd

--- plot3.R
+++ plot3.R
@@ -29,6 +29,6 @@ legend("topright", col = c("black", "red", "blue"), lty = 1,
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 # Create the PNG file
-dev.copy(png, file="plot3.png")
+dev.copy(png, file="plot3.png", width=480, height=480)
 dev.off()
 
