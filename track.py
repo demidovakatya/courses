ef34a5eea71a025ca8a6ee657728ba231112b2ae
Katya Demidova <demidovakatya@gmail.com>
2015-12-17 22:10:54 +0300

    Updated track: added missing table

--- track.py
+++ track.py
@@ -31,22 +31,26 @@ cur.executescript('''
     DROP TABLE IF EXISTS Artist;
     DROP TABLE IF EXISTS Album;
     DROP TABLE IF EXISTS Track;
+    DROP TABLE IF EXISTS Genre;
 
     CREATE TABLE Artist (
         id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
         name    TEXT UNIQUE
     );
-
+    CREATE TABLE Genre (
+        id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        name    TEXT UNIQUE
+    );
     CREATE TABLE Album (
         id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
         artist_id  INTEGER,
         title   TEXT UNIQUE
     );
-
     CREATE TABLE Track (
         id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
         title TEXT  UNIQUE,
         album_id  INTEGER,
+        genre_id  INTEGER,
         len INTEGER, rating INTEGER, count INTEGER
     );
 ''')
@@ -75,24 +79,28 @@ for entry in all:
     # so what should we look for?
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
+    genre = lookup(entry, 'Genre')
     album = lookup(entry, 'Album')
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
 
     # because fuck that it's not even a real track
-    if name is None or artist is None or album is None :
+    if name is None or artist is None or genre is None or album is None:
         continue
 
     # sharing our success
-    print name, artist, album, count, rating, length
+    print name, artist, genre, album, count, rating, length
 
-    # add to Artist
     cur.execute('''INSERT OR IGNORE INTO Artist (name)
         VALUES ( ? )''', ( artist, ) )
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     # Fetches the next row of a query result set, returning a single sequence, or None when no more data is available.
     artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
         VALUES ( ?, ? )''', ( album, artist_id ) )
@@ -100,10 +108,15 @@ for entry in all:
     album_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count)
-        VALUES ( ?, ?, ?, ?, ? )''',
-        ( name, album_id, length, rating, count ) )
+        (title, genre_id, album_id, len, rating, count)
+        VALUES ( ?, ?, ?, ?, ?, ? )''',
+        ( name, genre_id, album_id, length, rating, count ) )
 
-    # So now we have three tables
+
+    cur.execute('''SELECT Track.title, Artist.name, Album.title, Genre.name 
+    FROM Track JOIN Genre JOIN Album JOIN Artist 
+    ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
+        AND Album.artist_id = Artist.id
+    ORDER BY Artist.name LIMIT 3''')
 
     conn.commit()
