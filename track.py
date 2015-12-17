90362bf3721d7cf55e973c12d69ff4d73dffe32c
Katya Demidova <demidovakatya@gmail.com>
2015-12-17 21:37:59 +0300

    Upd track~~s~~
    = renamed the file
    + added db

--- track.py
+++ track.py
@@ -28,27 +28,27 @@ cur = conn.cursor()
 
 # Make some fresh tables using executescript()
 cur.executescript('''
-DROP TABLE IF EXISTS Artist;
-DROP TABLE IF EXISTS Album;
-DROP TABLE IF EXISTS Track;
+    DROP TABLE IF EXISTS Artist;
+    DROP TABLE IF EXISTS Album;
+    DROP TABLE IF EXISTS Track;
 
-CREATE TABLE Artist (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name    TEXT UNIQUE
-);
+    CREATE TABLE Artist (
+        id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        name    TEXT UNIQUE
+    );
 
-CREATE TABLE Album (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    artist_id  INTEGER,
-    title   TEXT UNIQUE
-);
+    CREATE TABLE Album (
+        id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        artist_id  INTEGER,
+        title   TEXT UNIQUE
+    );
 
-CREATE TABLE Track (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    title TEXT  UNIQUE,
-    album_id  INTEGER,
-    len INTEGER, rating INTEGER, count INTEGER
-);
+    CREATE TABLE Track (
+        id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        title TEXT  UNIQUE,
+        album_id  INTEGER,
+        len INTEGER, rating INTEGER, count INTEGER
+    );
 ''')
 
 fname = raw_input('Enter file name: ')
@@ -78,24 +78,24 @@ for entry in all:
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
 
-    if name is None or artist is None or album is None : 
+    if name is None or artist is None or album is None :
         continue
 
     print name, artist, album, count, rating, length
 
-    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+    cur.execute('''INSERT OR IGNORE INTO Artist (name)
         VALUES ( ? )''', ( artist, ) )
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
         VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''', 
+        (title, album_id, len, rating, count)
+        VALUES ( ?, ?, ?, ?, ? )''',
         ( name, album_id, length, rating, count ) )
 
     conn.commit()
