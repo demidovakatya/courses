d5edd75aec0b7f950c2782e529af69f6dffe97a9
Katya Demidova <demidovakatya@gmail.com>
2015-12-23 21:25:04 +0300

    Upd part 2

--- part2.R
+++ part2.R
@@ -3,13 +3,21 @@
 # 3. Use confidence intervals and/or hypothesis tests to compare tooth growth by supp and dose. (Only use the techniques from class, even if there's other approaches worth considering)
 # 4. State your conclusions and the assumptions needed for your conclusions. 
 
+# 1
 library(ggplot2)
 
 library(datasets)
 tg <- ToothGrowth
 str(tg)
+head(tg)
+summary(tg)
 
+tg$dose <- as.factor(tg$dose)
+str(tg)
 
+# 2
 ggplot(data = tg, aes(as.factor(dose), len)) + geom_point(aes(colour=supp), size=4) + ggtitle("") + facet_grid(. ~ supp) 
-ggplot(data = tg, aes(supp, len)) + geom_point(aes(colour=as.factor(dose)), size=4) + ggtitle("")
 
+# 3
+library(dplyr)
+tg %>% group_by(supp) %>% summarise(mean(len))
