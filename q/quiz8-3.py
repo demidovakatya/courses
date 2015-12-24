a3618416348f7c87a6e2b59af3a345e7ca3f0106
Katya Demidova <demidovakatya@gmail.com>
2015-12-24 17:36:04 +0300

    Lecture 8

---
+++ q/quiz8-3.py
@@ -0,0 +1,22 @@
+from Bio.Blast import NCBIWWW, NCBIXML
+
+sequence = "TGGGCCTCATATTTATCCTATATACCATGTTCGTATGGTGGCGCGATGTTCTACGTGAATCCACGTTCGAAGGACATCATACCAAAGTCGTACAATTAGGACCTCGATATGGTTTTATTCTGTTTATCGTATCGGAGGTTATGTTCTTTTTTGCTCTTTTTCGGGCTTCTTCTCATTCTTCTTTGGCACCTACGGTAGAG"
+
+result_h = NCBIWWW.qblast("blastn", "nt", sequence)
+blast_rec = NCBIXML.read(result_h)
+
+E_VALUE_THRESH = 0.01
+
+for alignment in blast_rec.alignments:
+    for hsp in alignment.hsps:
+        if hsp.expect < E_VALUE_THRESH:
+            print('****Alignment****')
+            print('sequence:', alignment.title)
+            print('length:', alignment.length)
+            print('e value:', hsp.expect)
+            print(hsp.query)
+            print(hsp.match)
+            print(hsp.sbjct)
+
+# ****Alignment****
+# sequence: gi|927029471|gb|KR780036.1| Nicotiana tabacum cultivar zhongyan 100 mitochondrion, complete genome
