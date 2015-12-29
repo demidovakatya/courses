3f3085dc483ae29cc185457eac4ef7ddf032d61d
Katya Demidova <demidovakatya@gmail.com>
2015-12-29 15:14:20 +0300

    find sequences, find ORFs

--- exam.py
+++ exam.py
@@ -1,34 +1,59 @@
 from Bio.Blast import NCBIWWW, NCBIXML
-
-name = "dna.example.fasta"
-f = open(name)
-
-
-# Count records
-text = f.read()
-records = text.count(">")
-print("There are %d records" % records)
+import re
+import sys
 
 
-# Find sequences
+if len(sys.argv) < 1:
+    name = "dna.example.fasta"
+else:
+    name = sys.argv[1]
+handle = open(name)
+
+
+records = 0
 seqs = []
 current_seq = ""
-for line in text:
+
+for line in handle:
+    # print(line)
     if not line.startswith(">"):
         current_seq += line.strip()
     else:
+        # print("Line doesn't startswith >")
         seqs.append(current_seq)
         current_seq = ""
-seqs.append(current_seq)
+        records += 1
+
+seqs.append(seq)
 seqs = seqs[1:]
+
+# Count records
+found_records = text.count(">")
+print("There are %d (or %d) records" % (records, found_records))
+
 
 # Find lengths
 seq_lengths = [len(seq) for seq in seqs]
+print("Length of the longest sequence: %d" % max(seq_lengths))
+print("Length of the shortest sequence: %d" % min(seq_lengths))
 
-# Find longest sequence
-print("Longest sequence is %d" % max(seq_lengths))
+# Find longest ORF
+def find_orf(sequence, number):
 
-# Find shortest sequence
-print("Shortest sequence is %d" % min(seq_lengths))
+    number = number - 1
 
-# Find longest ORF 2
+    starts = [a.start() for a in list(re.finditer('ATG', sequence[number:]))]
+    stops = [a.end() for a in list(re.finditer('TAA', sequence[number:]))] 
+    stops += [a.end() for a in list(re.finditer('TGA', sequence[number:]))] 
+    stops += [a.end() for a in list(re.finditer('TAG', sequence[number:]))]
+
+    orfs = []
+
+    for i in starts:
+        for j in stops:
+            if i > j:
+                continue
+            # print("String from %d to %d" % (i, j))
+            orfs.append(sequence[i:j])
+
+    return orfs
