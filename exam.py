bbc579385596e57b0b4a59af66986789cc7e08a4
Katya Demidova <demidovakatya@gmail.com>
2015-12-29 17:08:15 +0300

    something else was done

--- exam.py
+++ exam.py
@@ -3,16 +3,18 @@ import re
 import sys
 
 
-if len(sys.argv) < 1:
-    name = "dna.example.fasta"
-else:
-    name = sys.argv[1]
+# if len(sys.argv) < 1:
+#     name = "dna.example.fasta"
+# else:
+#     name = sys.argv[1]
 
+
+name = "dna.fasta"
 handle = open(name)
 
 
-records = {}
-seqs = []
+records = {} # {header: sequence}
+seqs = [] # [sequence]
 current_seq = ""
 
 for line in handle:
@@ -27,51 +29,84 @@ for line in handle:
 seqs.append(current_seq)
 seqs = seqs[1:]
 
+
 print("There are %d records" % (len(records)))
+print("Length of the longest sequence: %d" % max([len(seq) for seq in seqs]))
+print("Length of the shortest sequence: %d" % min([len(seq) for seq in seqs]))
 
 
-# Find lengths
-seq_lengths = [len(seq) for seq in seqs]
-print("Length of the longest sequence: %d" % max(seq_lengths))
-print("Length of the shortest sequence: %d" % min(seq_lengths))
+def find_orf(sequence, frame):
+    """Looks for ORFs – open reading frames
 
+    An open reading frame starts with a start codon, and ends with the next stop codon.
+    """
+    start_codons = ["ATG"]
+    stop_codons = ["TAA", "TGA", "TAG"]
+    # a_string = sequence[frame-1:]
 
-def find_orf(sequence, number):
+    # starts = [a.start() for a in list(re.finditer('ATG', a_string))]
 
-    a_string = sequence[number-1:]
+    # stops = [a.end() for a in list(re.finditer('TAA', a_string))] 
+    # stops += [a.end() for a in list(re.finditer('TGA', a_string))] 
+    # stops += [a.end() for a in list(re.finditer('TAG', a_string))]
 
-    starts = [a.start() for a in list(re.finditer('ATG', a_string))]
+    # starts.sort()
+    # stops.sort()
 
-    stops = [a.end() for a in list(re.finditer('TAA', a_string))] 
-    stops += [a.end() for a in list(re.finditer('TGA', a_string))] 
-    stops += [a.end() for a in list(re.finditer('TAG', a_string))]
+    starts = []
+    ends = []
+    for i in range(frame-1, len(sequence), 3):
+        if sequence[i:i+3] in start_codons:
+            starts.append(i)
+        if sequence[i:i+3] in stop_codons:
+            ends.append(i+2)
 
     orfs = []
 
     for i in starts:
-        for j in stops:
-            if i > j:
+        for j in ends:
+            if (i > j) or (j - i < 6):
                 continue
-            # print("String from %d to %d" % (i, j))
-            orfs.append(sequence[i:j])
-
+            print("String from %d to %d. Length: %d" % (i, j+1, j+1-i))
+            orfs.append(sequence[i:j+1])
+            break
+ 
     return orfs
 
 
 orfs1 = []
 for seq in seqs:
     orfs1 += find_orf(seq, 1)
-print("Length of the longest ORF: %d" % max([len(orf) for orf in orfs1]))
+print("Length of the longest ORF 1: %d" % max([len(orf) for orf in orfs1]))
 orfs2 = []
 for seq in seqs:
     orfs2 += find_orf(seq, 2)
-print("Length of the longest ORF: %d" % max([len(orf) for orf in orfs2]))
+print("Length of the longest ORF 2: %d" % max([len(orf) for orf in orfs2]))
 orfs3 = []
 for seq in seqs:
     orfs3 += find_orf(seq, 3)
-print("Length of the longest ORF: %d" % max([len(orf) for orf in orfs3]))
+print("Length of the longest ORF 3: %d" % max([len(orf) for orf in orfs3]))
 
 orfs = orfs1 + orfs2 + orfs3
 
 orf_lengths = [len(orf) for orf in orfs]
 print("Length of the longest ORF: %d" % max(orf_lengths))
+
+
+# What is the length of the longest forward ORF that appears in the sequence with the identifier gi|142022655|gb|EQ086233.1|16?
+identifier = "gi|142022655|gb|EQ086233.1|16"
+
+# Find the most frequently occurring repeat of length 6 in all sequences. 
+# How many times does it occur in all?
+
+
+# Find all repeats of length 12 in the input file. 
+# Let's use Max to specify the number of copies of the most frequent repeat of length 12. 
+# How many different 12-base sequences occur Max times?
+
+
+# Which one of the following repeats of length 7 has a maximum number of occurrences?
+# TGCGCGC
+# GCGCGCA
+# CGCGCCG
+# AATGGCA
