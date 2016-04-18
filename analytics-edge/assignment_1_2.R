c915812b3f43d954251cc4075b5c73d2f78a3753
Katya Demidova <demidovakatya@gmail.com>
2016-04-18 22:05:48 +0300

    upd analytics edge

---
+++ analytics-edge/assignment_1_2.R
@@ -0,0 +1,20 @@
+IBM <- read.csv("IBMStock.csv")
+GE <- read.csv("GEStock.csv")
+CocaCola <- read.csv("CocaColaStock.csv")
+ProcterGamble <- read.csv("ProcterGambleStock.csv")
+Boeing <- read.csv("BoeingStock.csv")
+
+IBM$Date <- as.Date(IBM$Date, "%m/%d/%y")
+GE$Date <- as.Date(GE$Date, "%m/%d/%y")
+CocaCola$Date <- as.Date(CocaCola$Date, "%m/%d/%y")
+ProcterGamble$Date <- as.Date(ProcterGamble$Date, "%m/%d/%y")
+Boeing$Date <- as.Date(Boeing$Date, "%m/%d/%y")
+
+mean(IBM$StockPrice)
+min(GE$StockPrice)
+max(CocaCola$StockPrice)
+median(Boeing$StockPrice)
+sd(ProcterGamble$StockPrice)
+
+plot(CocaCola$Date, CocaCola$StockPrice, type="l", col="red")
+lines(ProcterGamble$Date, ProcterGamble$StockPrice, col="blue")
