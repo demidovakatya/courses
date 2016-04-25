49682bb231c46f4beb7b0cfb9df9b074e0d66b05
Katya Demidova <demidovakatya@gmail.com>
2016-04-25 23:28:28 +0300

    add an-edge assignment 1-2

--- analytics-edge/assignment_1_2.R
+++ analytics-edge/assignment_1_2.R
@@ -18,3 +18,35 @@ sd(ProcterGamble$StockPrice)
 
 plot(CocaCola$Date, CocaCola$StockPrice, type="l", col="red")
 lines(ProcterGamble$Date, ProcterGamble$StockPrice, col="blue")
+
+plot(CocaCola$Date[301:432], 
+     CocaCola$StockPrice[301:432], 
+     type = "l", col = "red", ylim = c(0,210))
+lines(ProcterGamble$Date[301:432], 
+      ProcterGamble$StockPrice[301:432],
+      col="blue")
+lines(Boeing$Date[301:432], 
+      Boeing$StockPrice[301:432],
+      col="black")
+lines(GE$Date[301:432], 
+      GE$StockPrice[301:432],
+      col="purple")
+lines(IBM$Date[301:432], 
+      IBM$StockPrice[301:432],
+      col="orange")
+abline(v = as.Date(c("2000-01-01")), lty = 2)
+abline(v = as.Date(c("1997-09-01")), lty = 2)
+abline(v = as.Date(c("1997-11-01")), lty = 2)
+abline(v = as.Date(c("2004-01-01")), lty = 2)
+abline(v = as.Date(c("2005-12-31")), lty = 2)
+
+tapply(IBM$StockPrice, months(IBM$Date), mean)
+mean(IBM$StockPrice)
+
+which.max(tapply(CocaCola$StockPrice, months(CocaCola$Date), mean))
+which.max(tapply(GE$StockPrice, months(GE$Date), mean))
+
+tapply(CocaCola$StockPrice, months(CocaCola$Date), mean)
+tapply(GE$StockPrice, months(GE$Date), mean)
+
+rm(Boeing, CocaCola, GE, IBM, ProcterGamble)
--- analytics-edge/week_1.R
+++ analytics-edge/week_1.R
@@ -5,3 +5,5 @@ WHO$Country[183]
 
 # mean percentage of population over 60 in regions
 tapply(WHO$Over60, WHO$Region, mean)
+tapply(WHO$LiteracyRate, WHO$Region, min, na.rm = T)
+tapply(WHO$ChildMortality, WHO$Region, mean)
