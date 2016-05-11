b0c19e6442e54ca15ae88c30dd583f41dd3d8909
Katya Demidova <demidovakatya@gmail.com>
2016-05-11 03:00:23 +0300

    upd week 4 homework part 1

--- analytics-edge/week_4/assignment_4_1.R
+++ analytics-edge/week_4/assignment_4_1.R
@@ -34,3 +34,27 @@ prp(cart.model.2)
 cart.model.3 <- rpart(voting ~ civicduty + hawthorne + self + neighbors + sex + control, 
                     data = data, cp = 0.0)
 prp(cart.model.3)
+
+cart.model.control <- rpart(voting ~ control, cp = 0.0, data = data)
+prp(cart.model.control, digits = 6)
+abs(0.296638 - 0.34)
+
+cart.model.control.sex <- rpart(voting ~ control + sex, cp = 0.0, data = data)
+prp(cart.model.control.sex, digits = 6)
+abs(0.290456 - 0.334176)
+abs(0.302795 - 0.345818)
+
+log.model.sex <- glm(voting ~ sex + control, data = data, family = "binomial")
+summary(log.model.sex)
+
+possibilities <- data.frame(sex = c(0, 0, 1, 1), 
+                            control = c(0, 1, 0, 1))
+predict(log.model.sex, newdata = possibilities, type = "response")
+abs(0.290456 - 0.2908065)
+
+log.model.2 <- glm(voting ~ sex + control + sex:control, 
+                   data = data, family = "binomial")
+summary(log.model.2)
+
+predict(log.model.2, newdata = possibilities, type = "response")
+round(abs(0.290456 - 0.2904558), 5)
