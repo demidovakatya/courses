2af66ffbb52dae06a3edeeb699f15a015b33b942
Katya Demidova <demidovakatya@gmail.com>
2016-05-14 22:41:10 +0300

    upd homework 4 - finished

--- analytics-edge/week_4/assignment_4_3.R
+++ analytics-edge/week_4/assignment_4_3.R
@@ -4,6 +4,10 @@ library(ROCR)
 library(pROC)
 library(dplyr)
 library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(caret)
+library(e1071)
 data <- read.csv("census.csv")
 
 set.seed(2000)
@@ -11,30 +15,65 @@ spl <- sample.split(data$over50k, SplitRatio = 0.6)
 train <- subset(data, spl == TRUE)
 test <- subset(data, spl == FALSE)
 
-log.model.over50k <- glm(over50k ~ ., data = train, family = "binomial")
-summary(log.model.over50k)
+log.model <- glm(over50k ~ ., data = train, family = "binomial")
+summary(log.model)
 
-pred.over50k <- predict(log.model.over50k, type = "response", newdata = test)
-table(test$over50k, pred.over50k > 0.5) %>% diag %>% sum / nrow(test)
+log.pred <- predict(log.model, type = "response", newdata = test)
+table(test$over50k, log.pred > 0.5) %>% diag %>% sum / nrow(test)
 
 table(train$over50k)
 baseline <- rep("<=50K", nrow(test))
 table(test$over50k, baseline)[1] / nrow(test)
 
 # What is the area-under-the-curve (AUC) for this model on the test set?
-log.over50.rocr <- prediction(pred.over50k, test$over50k)
-(log.over50.auc <- as.numeric(performance(log.over50.rocr, "auc")@y.values))
+log.rocr <- prediction(log.pred, test$over50k)
+(log.auc <- as.numeric(performance(log.rocr, "auc")@y.values))
 
-cart.over50 <- rpart(over50k ~ ., data = train, method = "class")
-prp(cart.over50)
+cart <- rpart(over50k ~ ., data = train, method = "class")
+prp(cart)
 
-cart.pred.over50 <- predict(cart.over50, newdata = test, type = "class")
-table(test$over50k, cart.pred.over50) %>% diag %>% sum / nrow(test)
+cart.pred <- predict(cart, newdata = test, type = "class")
+table(test$over50k, cart.pred) %>% diag %>% sum / nrow(test)
 
 # What is the AUC of the CART model on the test set?
-cart.pred.over50 <- predict(cart.over50, newdata = test)
-cart.over50.rocr <- prediction(cart.pred.over50[, 2], test$over50k)
-(cart.over50.auc <- performance(cart.over50.rocr, "auc")@y.values)
-# auc(test$over50k, cart.pred.over50)
+cart.pred <- predict(cart, newdata = test)
+cart.rocr <- prediction(cart.pred[, 2], test$over50k)
+(cart.auc <- performance(cart.rocr, "auc")@y.values)
 
 set.seed(1)
+train.s <- train[sample(nrow(train), 2000), ]
+
+set.seed(1)
+rf <- randomForest(over50k ~ ., data = train.s)
+pred.rf <- predict(rf, newdata = test, type = "class")
+table(pred.rf, test$over50k) %>% diag %>% sum / nrow(test)
+
+# This code produces a chart that for each variable measures the number of times 
+# that variable was selected for splitting (the value on the x-axis). 
+vu <- varUsed(rf, count = T)
+vu.sorted <- sort(vu, decreasing = F, index.return = T)
+dotchart(vu.sorted$x, names(rf$forest$xlevels[vu.sorted$ix]))
+
+# A different metric we can look at is related to "impurity", which measures 
+# how homogenous each bucket or leaf of the tree is. 
+# In each tree in the forest, whenever we select a variable and perform a split, 
+# the impurity is decreased. 
+# Therefore, one way to measure the importance of a variable is to average 
+# the reduction in impurity, taken over all the times that variable is selected 
+# for splitting in all of the trees in the forest. 
+varImpPlot(rf)
+
+set.seed(2)
+tr.control <- trainControl(method = "cv", number = 10)
+grid <- expand.grid( .cp = seq(0.002, 0.1, 0.002))
+cv <- train(over50k ~ ., data = train,
+            method = "rpart",
+            trControl = tr.control,
+            tuneGrid = grid)
+cv$bestTune
+
+(best.tree <- cv$finalModel)
+best.tree.pred <- predict(cv, newdata = test)
+table(test$over50k, best.tree.pred) %>% diag %>% sum / nrow(test)
+
+best.tree %>% prp
