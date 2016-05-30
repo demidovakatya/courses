80530585115d25deaff768f042b1d97727989c9a
Katya Demidova <demidovakatya@gmail.com>
2016-05-30 22:29:24 +0300

    upd week 7 lectures (part 2)

--- analytics-edge/week_7/lectures_week_7.R
+++ analytics-edge/week_7/lectures_week_7.R
@@ -18,3 +18,31 @@ ggplot(who, aes(x = GNI, y = FertilityRate, color = Region)) + geom_point()
 ggplot(who, aes(x = GNI, y = FertilityRate, color = LifeExpectancy)) + geom_point()
 
 ggplot(who, aes(x = FertilityRate, y = Under15, color = Region)) + geom_point()
+
+# The Analytical Policeman: Visualization for Law and Order
+
+mvt <- read.csv("mvt.csv", stringsAsFactors = F)
+
+mvt$Date <- strptime(mvt$Date, format = "%m/%d/%y %H:%M")
+mvt$Weekday <- weekdays(mvt$Date)
+mvt$Hour <- mvt$Date$hour
+
+weekday.crimes <- data.frame(table(mvt$Weekday))
+weekday.crimes$Var1 <- factor(weekday.crimes$Var1, ordered = T, 
+                              levels = c("Monday", "Tuesday", "Wednesday",
+                                         "Thursday", "Friday", "Saturday", "Sunday"))
+ggplot(weekday.crimes, aes(x = Var1, y = Freq)) + geom_line(aes(group = 1)) +
+    xlab("day of the week") + ylab("total motor vehicle thefts")
+# ggplot(weekday.crimes, aes(x = Var1, y = Freq)) + geom_line(aes(group = 1), linetype = 2) +
+#    xlab("day of the week") + ylab("total motor vehicle thefts")
+
+hour.weekday.crimes <- data.frame(table(mvt$Weekday, mvt$Hour))
+hour.weekday.crimes$Hour <- as.numeric(as.character(hour.weekday.crimes$Var2))
+hour.weekday.crimes$Var1 <- factor(hour.weekday.crimes$Var1, ordered = T, 
+                                   levels = c("Monday", "Tuesday", "Wednesday",
+                                              "Thursday", "Friday", "Saturday", "Sunday"))
+ggplot(hour.weekday.crimes, aes(x = Hour, y = Freq)) + geom_line(aes(group = Var1, color = Var1)) +
+    xlab("hour of the day") + ylab("total motor vehicle thefts")
+ggplot(hour.weekday.crimes, aes(x = Hour, y = Var1)) + geom_tile(aes(fill = Freq)) +
+    scale_fill_gradient(name = "thefts", low = "white", high = "red") + 
+    theme(axis.title.y = element_blank())
