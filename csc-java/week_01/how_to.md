171335786668cbf70f46ca3cece3298f1a0156e2
Katya Demidova <demidovakatya@gmail.com>
2016-09-18 01:09:16 +0300

    added files to csc-java

---
+++ csc-java/week_01/how_to.md
@@ -0,0 +1,6 @@
+```
+~/Documents> cd _courses/csc-java/week_01/
+~/D/_/c/week_01> cd quiz/
+~/D/_/c/w/quiz> javac Quiz.java 
+~/D/_/c/w/quiz> java Quiz
+```
---
+++ csc-java/week_01/quiz/Quiz.java
@@ -0,0 +1,12 @@
+import java.security.MessageDigest;
+
+public class Quiz {
+
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
+    }
+}
