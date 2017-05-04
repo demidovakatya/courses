d69789c8a99c0d289a8bad118f146ce267a35528
Katya Demidova <demidovakatya@gmail.com>
2017-05-05 00:26:53 +0300

    removing Coursera-related materials

--- genpython/q/quiz4-5.py
+++
@@ -1,4 +0,0 @@
-seq = "hello"
-for i in range(len(seq) + 1) :    # line 1 
-    for j in range(i) :        # line 2 
-        print(seq[j:i])     # line 3 
--- python-for-everybody/python-network-data/python-installed.py
+++
@@ -1,1 +0,0 @@
-print "Oh, look, I've installed Python."
